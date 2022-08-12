58ca8cdd3a176698769e433bc8ca4aa574eef566
Chris Barker <chris.barker@nice.org.uk>
2022-08-12 11:29:55 +0100

    Prettier, Stylelint, VSCode settings

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>DLG Alpha</title>
-        <meta name="description" content="Prototype front end build for Digital Living Guidelines" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="container container--full">
+			<Head>
+				<title>DLG Alpha</title>
+				<meta
+					name="description"
+					content="Prototype front end build for Digital Living Guidelines"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to DLG
-        </h1>
-      </main>
-    </div>
-  )
+			<main>
+				<h1>DLG Prototype</h1>
+				<p className={styles.intro}>Under construction!</p>
+			</main>
+		</div>
+	);
 }
