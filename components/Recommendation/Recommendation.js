6ffff21ff3e54387e24b512377982c80c55599c9
Chris Barker <chris.barker@nice.org.uk>
2022-08-31 16:45:46 +0100

    Basic recommendation styling

--- components/Recommendation/Recommendation.js
+++ components/Recommendation/Recommendation.js
@@ -4,14 +4,18 @@ import styles from "./Recommendation.module.scss";
 export const Recommendation = () => {
 	return (
 		<div className={styles.recommendation}>
-			<span>NG101-1.8.4</span>
-			<span>Updated: 18 July 2018</span>
-			<p>
-				Offer adjuvant trastuzumab if the person has early HER2-positive
-				invasive breast cancer and a tumour that is larger than 1 cm (T1c and
-				above).
-			</p>
-			<ul>
+			<div className={styles.id}>NG101-1.8.4</div>
+			<div className={styles.updated}>
+				Updated: <time>18 July 2018</time>
+			</div>
+			<div className={styles.description}>
+				<p>
+					Offer adjuvant trastuzumab if the person has early HER2-positive
+					invasive breast cancer and a tumour that is larger than 1 cm (T1c and
+					above).
+				</p>
+			</div>
+			<ul className={styles.linkList}>
 				<li>
 					<Link href="/">Evidence</Link>
 				</li>
