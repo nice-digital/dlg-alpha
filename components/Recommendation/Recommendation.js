871dfd09027fc9970684637cd2eeb9d30cb7a3d2
Chris Barker <chris.barker@nice.org.uk>
2022-09-01 11:02:06 +0100

    Flesh out recommendation props

--- components/Recommendation/Recommendation.js
+++ components/Recommendation/Recommendation.js
@@ -1,31 +1,40 @@
 import Link from "next/link";
 import styles from "./Recommendation.module.scss";
 
-export const Recommendation = () => {
+export const Recommendation = ({
+	children,
+	id,
+	dateUpdated,
+	evidenceLink,
+	updateLink,
+	sdmLink,
+}) => {
 	return (
 		<div className={styles.recommendation}>
-			<div className={styles.id}>NG101-1.8.4</div>
+			<div className={styles.id}>{id}</div>
 			<div className={styles.updated}>
-				Updated: <time>18 July 2018</time>
+				Updated: <time>{dateUpdated}</time>
 			</div>
-			<div className={styles.description}>
-				<p>
-					Offer adjuvant trastuzumab if the person has early HER2-positive
-					invasive breast cancer and a tumour that is larger than 1 cm (T1c and
-					above).
-				</p>
-			</div>
-			<ul className={styles.linkList}>
-				<li>
-					<Link href="/">Evidence</Link>
-				</li>
-				<li>
-					<Link href="/">Update information</Link>
-				</li>
-				<li>
-					<Link href="/">Shared decision making</Link>
-				</li>
-			</ul>
+			<div className={styles.description}>{children}</div>
+			{(evidenceLink || updateLink || sdmLink) && (
+				<ul className={styles.linkList}>
+					{evidenceLink && (
+						<li>
+							<Link href={evidenceLink}>Evidence</Link>
+						</li>
+					)}
+					{updateLink && (
+						<li>
+							<Link href={updateLink}>Update information</Link>
+						</li>
+					)}
+					{sdmLink && (
+						<li>
+							<Link href={sdmLink}>Shared decision making</Link>
+						</li>
+					)}
+				</ul>
+			)}
 		</div>
 	);
 };
--- pages/recommendations.js
+++ pages/recommendations.js
@@ -21,22 +21,35 @@ export default function Recommendations() {
 					<Breadcrumb to="/">Overview</Breadcrumb>
 					<Breadcrumb>Recommendations</Breadcrumb>
 				</Breadcrumbs>
+
+				<h1>Treating early HER2-positive invasive breast cancer</h1>
+
 				<Grid gutter="loose">
 					<GridItem cols={12} md={4} lg={3}>
 						Menu here!
 					</GridItem>
 					<GridItem cols={12} md={8} lg={9}>
-						<h1>Treating early HER2-positive invasive breast cancer</h1>
-						<p>
-							<Link href="/evidence">Evidence page</Link>
-						</p>
-						<Recommendation />
-						<ul>
-							<li>Lorem ipsum dolor sit amet,</li>
-							<li>consectetur adipiscing elit.</li>
-							<li>Aenean euismod bibendum laoreet.</li>
-							<li>Proin gravida dolor sit amet lacus accumsan</li>
-						</ul>
+						<h2>When to offer adjuvant trastuzumab</h2>
+
+						<h3>For tumours that are T1c and above</h3>
+						<Recommendation
+							id="NG101-1.8.4"
+							dateUpdated="18 July 2022"
+							evidenceLink="/evidence"
+							updateLink="/update"
+							sdmLink="/sdm"
+						>
+							<p>
+								Offer adjuvant trastuzumab if the person has early HER2-positive
+								invasive breast cancer and a tumour that is larger than 1 cm
+								(T1c and above).
+							</p>
+						</Recommendation>
+
+						<h3>For tumours that are T1a or T1b</h3>
+						<Recommendation id="NG101-1.8.5" dateUpdated="19 July 2022">
+							<p>A very simple recommendation with no additional links</p>
+						</Recommendation>
 					</GridItem>
 				</Grid>
 			</div>
